3c5f00f1a6aa1ebf70f6d9990c2a9bef352eb6d1
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-04-05 20:33:54 +0900

    barba.jsのmarkup
    barba.jsはいったんcdnで読み込み

--- _static/src/assets/js/common.js
+++ _static/src/assets/js/common.js
@@ -1,5 +1,21 @@
-import jQuery from 'jquery';
-const $ = jQuery;
-
-require('./modules/humburgerBtn');
-require('./modules/mvHeight');
+// barba設定
+barba.init({
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
+        console.log("leave");
+        //   leaveAnimation();
+        //   pageTransition();
+        //   await delay(1000);
+        done();
+      },
+      async enter(data) {
+        console.log("enter");
+        //   await delay(600);
+        //   enterAnimation();
+      },
+    },
+  ],
+  // ...
+});
